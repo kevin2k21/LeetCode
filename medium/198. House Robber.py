88ccf9b890531edcbe74853185be1659b7973382
Alonzo A Kevin <alonzoak2007@gmail.com>
2026-01-10 16:41:21 +0530

    small change

--- medium/198. House Robber.py
+++ medium/198. House Robber.py
@@ -5,7 +5,7 @@ class Solution:
         n = len(nums)
         mon = [0] * n
         mon[0] = nums[0]
-        mon[1] = nums[1]
+        mon[1] = max(nums[1],nums[0])
         for i in range(2,n):
             mon[i] = max(mon[i-1],nums[i]+mon[i-2])
         return mon[-1]
