677796de5e6cf161001bdf192ec81cc5085aae5a
Alonzo A Kevin <alonzoak2007@gmail.com>
2026-01-17 13:41:12 +0530

    optimizing 3047. Find the Largest Area of Square Inside Two Rectangles.py

--- medium/3047. Find the Largest Area of Square Inside Two Rectangles.py
+++ medium/3047. Find the Largest Area of Square Inside Two Rectangles.py
@@ -2,27 +2,30 @@ from typing import List
 
 class Solution:
     def largestSquareArea(self, bottomLeft: List[List[int]], topRight: List[List[int]]) -> int:
-        comb = list(zip(bottomLeft,topRight))
-        flat = [tuple(a + b) for a, b in comb]
-
-        l = len(comb)
+        max_len = 0
         max_area = 0
-        for i in range(l):
-            for j in range(i+1,l):
-
-                i_x_cords = (flat[i][0],flat[i][2])
-                i_y_cords = (flat[i][1],flat[i][3])
-                j_x_cords = (flat[j][0],flat[j][2])
-                j_y_cords = (flat[j][1],flat[j][3])
-
-                inter_x = min(i_x_cords[1],j_x_cords[1]) - max(i_x_cords[0],j_x_cords[0]) 
-                inter_y = min(i_y_cords[1],j_y_cords[1]) - max(i_y_cords[0],j_y_cords[0]) 
+        for i in range(len(bottomLeft)):
+            xi1 , yi1 = bottomLeft[i]
+            xi2 , yi2 = topRight[i]
+            if xi2 - xi1 < max_len or yi2 - yi1 < max_len:
+                continue
+            for j in range(i+1,len(bottomLeft)):
+                xj1 , yj1 = bottomLeft[j]
+                xj2 , yj2= topRight[j]
+                if xj2 - xj1 < max_len or yj2 - yj1 < max_len:
+                    continue
+                
+                inter_x = min(xj2,xi2) - max(xj1,xi1)
+                inter_y = min(yj2,yi2) - max(yj1,yi1)
 
                 if inter_x <= 0 or inter_y <= 0:
                     continue
 
-                max_area = max(max_area,min(inter_x,inter_y)**2)
-                    
+                max_len = max(max_len,min(inter_x,inter_y))
+
+            max_area = max_len**2
+            
         return max_area
+    
 sol = Solution()
 print(sol.largestSquareArea([[1,1],[2,2],[3,1]],[[3,3],[4,4],[6,6]]))
