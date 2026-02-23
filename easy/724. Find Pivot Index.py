66dff4462a9960853fd32729905b418e0f475012
Alonzo A Kevin <alonzoakevin@Alonzos-MacBook-Pro.local>
2026-02-23 17:30:40 +0530

    724. Find Pivot Index.py

---
+++ easy/724. Find Pivot Index.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total_sum = sum(nums)
+        left_sum = 0
+        for i in range(len(nums)):
+            if left_sum == total_sum - nums[i] - left_sum:
+                return i
+            left_sum += nums[i]
+        return -1
+sol = Solution()
+print(sol.pivotIndex([1,7,3,6,5,6]))
